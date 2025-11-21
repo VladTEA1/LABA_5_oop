61ed00e8027d711b4482591d5719da25b3d40364
VladTEA1 <161972729+VladTEA1@users.noreply.github.com>
2025-11-21 22:26:49 +0300

    Update complex_type.h

--- include/complex_type.h
+++ include/complex_type.h
@@ -2,15 +2,15 @@
 #define COMPLEX_TYPE_H
 
 #include <string>
-
+//09876543
 struct DataStruct {
     int identifier;
     std::string title;
     double numerical_value;
-    
+    //=-098765432
     DataStruct(int id = 0, const std::string& t = "", double val = 0.0) 
         : identifier(id), title(t), numerical_value(val) {}
-                                                                                                                                                                                                                                            
+    //-0987698754                                                                                                                                                                                                                             
     bool operator==(const DataStruct& other) const {
         return identifier == other.identifier && title == other.title && numerical_value == other.numerical_value;
     }
@@ -18,3 +18,4 @@ struct DataStruct {
 
 
 #endif
+
